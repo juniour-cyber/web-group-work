952abea7600919275e02aad5793862bfe76348a1
Juniour Msafiri <juniourmsafiri@gmail.com>
2026-06-08 10:11:26 +0300

    added comments

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,18 @@
-
+//this is an arrow function used instead of the traditional function writing style.
 document.addEventListener('DOMContentLoaded', () => {
+    // This event waits until the whole HTML page is fully loaded
+// before running any JavaScript. This prevents errors if elements
+// are not yet available in the DOM (Document Object Model).
     // 1. DIGITAL CLOCK LOGIC
     const clockDisplay = document.getElementById('digital-clock');
+     /*if condition ensures this code is skipped
+     and no crash occurs incase there is no digital-clock id element*/ 
     if (clockDisplay) {
+        //runs the function repeatedly in the given time which in this case is 1 sec
         setInterval(() => {
+//create a new time object which holds the current time
             const now = new Date();
+            //convert the time into a readable format
             clockDisplay.textContent = now.toLocaleTimeString();
         }, 1000);
     }
@@ -18,12 +26,15 @@ document.addEventListener('DOMContentLoaded', () => {
     if (welcomeElement) {
         welcomeElement.textContent = ""; // Clear fallback block text
         function typeWriter() {
+            //enables termination of my typewriter function, this only has an effect on performance
             if (index < welcomeMessage.length) {
                 welcomeElement.textContent += welcomeMessage.charAt(index);
                 index++;
+                //this is the line which calls the function after the short delay.
                 setTimeout(typeWriter, typingSpeed);
             }
         }
+        //starts the actual function implementation
         typeWriter();
     }
 });
